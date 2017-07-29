91ea3e4edbf123e9ee68955950e88005fcfc4e2e
Kevin Souto <soutokevin@gmail.com>
2017-07-28 22:56:18 -0300

    Carousel simples

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,30 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).on('turbolinks:load', function() {
+  $('#menu-btn').click(function() {
+    $('.responsive-menu').toggleClass('expand')
+    $('.menu-btn span').toggleClass('active')
+  })
 
-$( document ).on('turbolinks:load', function() {
-	$( '#menu-btn' ).click(function(){
-		$('.responsive-menu').toggleClass('expand')
-		$('.menu-btn span').toggleClass('active')
- 	})
-});
+  const carousel = $('.carousel')
+  const slides = carousel.find('.carousel__slide')
+  const controls = carousel.find('.carousel__control')
+
+  controls.click(function() {
+    const index = controls.index(this)
+
+    const nextSlide = $(slides[index])
+    const nextControl = $(controls[index])
+
+    if (nextSlide.length === 0) {
+      throw new Error('Slide não encontrado!!!')
+    }
+
+    slides.removeClass('carousel__slide--active')
+    controls.removeClass('carousel__control--active')
+
+    nextSlide.addClass('carousel__slide--active')
+    nextControl.addClass('carousel__control--active')
+  })
+})
