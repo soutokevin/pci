ac9ee1e4bb6378363a3f2e4c1a36308edd98a9cf
Danilo de Lima <danilodelyima@gmail.com>
2017-07-29 17:02:05 -0300

    Implementa seed da home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+page = Page.create(name: 'Home Page')
+page_part = page.page_parts.create([{name:'Seção 01', allow_creatable: false},
+																		{name:'Seção 02', allow_creatable: false},
+																		{name:'Seção 03', allow_creatable: false},
+																		{name:'Seção 04', allow_creatable: true},
+																		{name:'Seção 05', allow_creatable: true},
+																		{name:'Seção 06', allow_creatable: true},
+																		{name:'Seção 07', allow_creatable: true},
+																		{name:'Seção 08', allow_creatable: true},
+																		{name:'Seção 09', allow_creatable: false},
+																		{name:'Seção 10', allow_creatable: true},
+																		{name:'Seção 11', allow_creatable: true},]) 
