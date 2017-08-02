923986515a4245d851c76d28487162f0d19395a3
Danilo de Lima <danilodelyima@gmail.com>
2017-08-02 17:52:14 -0300

    Adiciona métodos para recuperar page parts e id das páginas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+	def services(index)
+		@services_name = ['management', 'engineering', 'juridical']
+		@services_name[index]
+	end
+
+	def page_part(page, name)
+		page.page_parts.each do |page_part|
+			if page_part.name == name
+				return page_part
+			end 
+		end
+	end
+
+	def redirect_page(name)
+		pages = { "Home Page" => 1, "Client" => 2, "Student" => 3 }
+		pages[name]
+	end
 end
