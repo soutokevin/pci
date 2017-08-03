24d1df9143a9022279f202dda27d8680a215a01d
Danilo de Lima <danilodelyima@gmail.com>
2017-08-03 15:34:23 -0300

    Valida presença na crianção de conteúdo

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,20 @@
 class Content < ApplicationRecord
 	has_attachment  :avatar, accept: [:jpg, :png, :gif]
 	belongs_to :page_part, optional: true
+
+	validates_presence_of :avatar, if: 'allow_photo?'
+	validates_presence_of :title, if: 'allow_title?'
+	validates_presence_of :body, if: 'allow_text?'
+
+	def allow_photo?
+  	self.page_part.allow_photo
+	end
+
+	def allow_title?
+  	self.page_part.allow_title
+	end
+
+	def allow_text?
+  	self.page_part.allow_text
+	end
 end
