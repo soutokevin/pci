13cfb705d8c5556150c58e8e7ff89fb27f3dc566
Danilo de Lima <danilodelyima@gmail.com>
2017-08-04 16:21:56 -0300

    Adiciona estilo para tela de login

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,21 @@
 
 
 $( document ).on('turbolinks:load', function() {
+	// Comportamento do Hamburguer
 	$( '#menu-btn' ).click(function(){
 		$('.responsive-menu').toggleClass('expand')
 		$('.menu-btn span').toggleClass('active')
  	})
+ 	// Inicializa attachinary input para upload de imagens
  	$('.attachinary-input').attachinary()
+ 	// Comportamento do da página de Login
+ 	var working = false;
+	$('.login').on('submit', function(e) {
+	  if (working) return;
+	  working = true;
+	  var $this = $(this),
+	    $state = $this.find('button > .state');
+	  $this.addClass('loading');
+	  $state.html('Authenticating');
+	});
 });
