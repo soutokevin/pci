89b4e0803fb63a215c143d7910cfe75b62ea3a4d
Kevin Souto <soutokevin@gmail.com>
2017-08-08 12:39:05 -0300

    Termina o modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -14,4 +14,24 @@ $(document).on('turbolinks:load', function() {
     body.addClass('with-active-modal')
     modal.addClass('modal--active')
   })
+
+  function setup(opts) {
+    modal.find('.modal__title').text(opts.title)
+
+    var text = modal.find('.modal__text')
+    var images = modal.find('.modal__images')
+
+    // Clean everything up
+    text.children().remove()
+    images.children().remove()
+
+    text.html(opts.description)
+
+    opts.images.forEach(function(src) {
+      var img = $('<img class="modal__image" src="' + src + '">')
+      images.append(img)
+    })
+  }
+
+  window.setModal = setup
 })
