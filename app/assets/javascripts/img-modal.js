94fa285f91a3273bdfacc492e49a8f6769c9cdb4
Kevin Souto <soutokevin@gmail.com>
2017-08-10 22:41:39 -0300

    Lista de fotos na pparte inferior

--- app/assets/javascripts/img-modal.js
+++ app/assets/javascripts/img-modal.js
@@ -2,6 +2,7 @@ $(document).on('turbolinks:load', function() {
   var body = $(document.body),
     modal = $('#images'),
     preview = modal.find('.modal__preview'),
+    list = modal.find('.modal__list'),
     current,
     images
 
@@ -11,10 +12,26 @@ $(document).on('turbolinks:load', function() {
     }
   })
 
+  function setupList() {
+    // Clean up
+    list.children().remove()
+
+    images.forEach(function(src, index) {
+      var img = $('<img class="modal__item" src="' + src + '">')
+
+      if (current === index) {
+        img.addClass('modal__item--active')
+      }
+
+      list.append(img)
+    })
+  }
+
   window.showImageModal = function setup(list, index) {
     current = index
     images = list
 
+    setupList()
     preview.attr('src', images[current])
     modal.addClass('modal--active')
   }
